4abb8f4f7b8f66ea9b092365dffd0acd0f0b7aaa
Yusuf <yusuf56senyuz@gmail.com>
2021-04-11 19:00:02 +0300

    Update MonthActivity.java

--- app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
+++ app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
@@ -14,18 +14,14 @@ public class MonthActivity extends BaseActivity {
 
         // DBHelper is not finished yet. Comment out if causes problems
         // DBHelper dbHelper = new DBHelper(MainActivity.this, null, null);
-<<<<<<< HEAD
+
         // Merhaba ben yusuf
         // deneme 123 r
-=======
-<<<<<<< Updated upstream
 
-=======
         // Merhaba ben yusuf
         // deneme 123 r
         // yusuf abem
->>>>>>> Stashed changes
->>>>>>> Abuzer
+
 
     }
 
