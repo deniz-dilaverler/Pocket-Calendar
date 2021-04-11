5e459b40d8eb72fa7da1de812bc7cc1ef1f8e21b
AbuzerAQ <dmertdilaverler@gmail.com>
2021-04-11 19:02:42 +0300

    Update MonthActivity.java

--- app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
+++ app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
@@ -9,23 +9,13 @@ public class MonthActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_month);
         super.onCreate(savedInstanceState);
-//sdklfhnksdjfhasdjkl
+
 
 
         // DBHelper is not finished yet. Comment out if causes problems
         // DBHelper dbHelper = new DBHelper(MainActivity.this, null, null);
 
-        // Merhaba ben yusuf
-        // deneme 123 r
-
-        // Merhaba ben yusuf
-        // deneme 123 r
-        // yusuf abem
-
-        // asdjfjkoldashgdfhasguhsdfıoşgjmsdrf
-
-
-
+        
     }
 
 
