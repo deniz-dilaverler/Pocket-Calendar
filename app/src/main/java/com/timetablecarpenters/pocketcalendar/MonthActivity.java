c03af25cc5b02f69ec278a79c8b5eded36c40da1
yarkinn <yarkinnsakinci@gmail.com>
2021-04-11 21:20:40 +0300

    Update MonthActivity.java

--- app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
+++ app/src/main/java/com/timetablecarpenters/pocketcalendar/MonthActivity.java
@@ -14,7 +14,7 @@ public class MonthActivity extends BaseActivity {
 
         // DBHelper is not finished yet. Comment out if causes problems
         // DBHelper dbHelper = new DBHelper(MainActivity.this, null, null);
-        // asdfgklşasdhfasdhf
+        // mrb
 
     }
 
