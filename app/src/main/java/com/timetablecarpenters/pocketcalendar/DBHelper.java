7fa9da9fc3c55b4a7c0b8f26e64630c8ec4951d6
AbuzerAQ <dmertdilaverler@gmail.com>
2021-04-12 20:16:43 +0300

    Update DBHelper.java

--- app/src/main/java/com/timetablecarpenters/pocketcalendar/DBHelper.java
+++ app/src/main/java/com/timetablecarpenters/pocketcalendar/DBHelper.java
@@ -2,6 +2,7 @@ package com.timetablecarpenters.pocketcalendar;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -9,16 +10,19 @@ import androidx.annotation.Nullable;
 
 public class DBHelper extends SQLiteOpenHelper {
 
-    private static final String EVENTS_TABLE = "events_table";
     private static final String YEAR = "year";
     private static final String MONTH = "month";
     private static final String DAY = "day";
+    private static final String EVENTS_TABLE = "events_table";
     private static final String EVENT_TYPE = "event_type";
     private static final String EVENT_NAME = "event_name";
+    private static final String EVENT_START = "event_name";
+    private static final String EVENT_END = "event_name";
     private static final String NOTES = "notes";
     private static final String LATITUDE = "latitude";
     private static final String LONGITUDE = "longitude";
     private static final String NOTIF_TIME = "notification_time";
+
 
 
 
@@ -32,13 +36,14 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         String createTableStatement = "CREATE TABLE " + EVENTS_TABLE + " ( " +     
-                YEAR + " INTEGER," +
-                MONTH + " INTEGER, " +
-                DAY + " INTEGER, " +
+
                 EVENT_TYPE + " TEXT, " +
                 EVENT_NAME + " TEXT," +
-
-
+                YEAR + " INTEGER," +
+                MONTH + " INTEGER," +
+                DAY + " INTEGER," +
+                EVENT_START + " TEXT," +
+                EVENT_END + " TEXT," +
                 LONGITUDE + " REAL," +
                 LATITUDE + " REAL," +
                 NOTES + " TEXT, " +
@@ -48,27 +53,58 @@ public class DBHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
 
     }
-/*
-    public boolean addOne(CalenderEvent event) {
+
+    /**
+     * adds the Data of an event object into the DB and returns the success status of the method as a long value
+     * @param event
+     * @return row number if event is succesful, -1 if an error has occured, -2 if the event already exists
+     */
+    public long insertEvent(CalenderEvent event) {
         ContentValues cv = new ContentValues();
         SQLiteDatabase db = this.getWritableDatabase();
 
-        cv.put(YEAR, event.getYear());
-        cv.put(MONTH, event.getMonth());
-        cv.put(DAY, event.getDay());
-        cv.put(EVENT_TYPE, event.getType());
-        cv.put(EVENT_NAME, event.getName());
-        cv.put(NOTES, event.getNotes());
-        cv.put(LONGITUDE, event.getLogitude());
-        cv.put(LATITUDE, event.getLatitued());
-        cv.put(NOTIF_TIME, event.getNotifTime());
+        if (checkIsDataAlreadyInDB(event)) {
+            cv.put(YEAR, event.getYear());
+            cv.put(MONTH, event.getMonth());
+            cv.put(DAY, event.getDay());
+            cv.put(EVENT_TYPE, event.getType());
+            cv.put(EVENT_NAME, event.getName());
+            cv.put(NOTES, event.getNotes());
+            cv.put(LONGITUDE, event.getLogitude());
+            cv.put(LATITUDE, event.getLatitued());
+            cv.put(NOTIF_TIME, event.getNotifTime());
+            cv.put(EVENT_START, event.getEventStart());
+            cv.put(EVENT_END, event.getEventEnd());
 
-        long insert = db.insert(EVENTS_TABLE, null, cv);
-        return insert <= 0;
+            long insert = db.insert(EVENTS_TABLE, null, cv);
+            return insert;
+        }
+        else
+            return -2;
+
     }
-*/
+
+    public boolean checkIsDataAlreadyInDB( CalenderEvent event) {
+        SQLiteDatabase db = getReadableDatabase();
+
+        String Query = "Select * from " + EVENTS_TABLE + " where " + YEAR + " = " + event.getYear() + " AND "
+                                                                   + MONTH + " = " + event.getMonth() + " AND "
+                                                                   + DAY + " = " + event.getDay() + " AND "
+                                                                   + EVENT_NAME + " = " + event.getName() + " AND "
+                                                                   + EVENT_START + " = " + event.getStart() + " AND "
+                                                                   + EVENT_END + " = " + event.getEnd() + " ;"
+                ;
+        Cursor cursor = db.rawQuery(Query, null);
+        if(cursor.getCount() <= 0){
+            cursor.close();
+            return false;
+        }
+        cursor.close();
+        return true;
+    }
+
     
 }
