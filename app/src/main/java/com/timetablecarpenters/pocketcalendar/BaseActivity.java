974f1a74186d195c150e515e5b65ee427093d30c
Diadex <alperenutku2002@gmail.com>
2021-05-02 17:05:11 +0300

    Revert "Revert "Revert "j"""
    This reverts commit 878125c8e471514d735265c57a5c82c83ba53641.

--- app/src/main/java/com/timetablecarpenters/pocketcalendar/BaseActivity.java
+++ app/src/main/java/com/timetablecarpenters/pocketcalendar/BaseActivity.java
@@ -95,7 +95,7 @@ public class BaseActivity extends AppCompatActivity implements GestureDetector.O
                 intent = new Intent(this, SettingsActivity.class);
                 break;
             case R.id.customization:
-                intent = new Intent(this, CustomizableScreenUnused.class);
+                intent = new Intent(this, CustomizableScreen.class);
                 break;
             case R.id.About_Devs:
                 //todo: this is for testing purposes, remove in final version
